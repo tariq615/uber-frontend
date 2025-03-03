64f375aad573936a40311d1e13508e30eb9d0fbf
tariq615 <iqbaltariq615@gmail.com>
2025-03-03 08:31:03 +0300

    updated name

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -8,7 +8,7 @@ const Start = () => {
       <div className="bg-cover bg-center bg-[url(/images/self/startbg.webp)] h-screen pt-8 flex justify-between flex-col w-full">
         <StartLogo />
         <div className="bg-white pb-8 py-4 px-4">
-          <h2 className="text-[30px] font-semibold">Get Started with Uber</h2>
+          <h2 className="text-[30px] font-semibold">Get Started with Sawari</h2>
           <Link
             to="/login"
             className="flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5"
