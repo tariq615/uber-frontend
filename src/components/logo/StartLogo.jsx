9e98cb637bc6e57e1671d5074b9aead2e92d00f3
tariq615 <iqbaltariq615@gmail.com>
2025-03-03 08:27:59 +0300

    logo updated in start

--- src/components/logo/StartLogo.jsx
+++ src/components/logo/StartLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StartLogo = () => {
   return (
-    <img className='w-16 ml-8' src="https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417" alt="" />
+    <img className='w-24 -mt-10 scale-150' src="/public/images/self/sawari.png" alt="" />
   )
 }
 
